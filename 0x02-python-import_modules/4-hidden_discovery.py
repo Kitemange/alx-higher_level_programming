ab8c9d5f8700ddd087816bfcf2b923a6a60d4da5
Kitemange <bryanmutheke18@gmail.com>
2022-06-09 23:51:24 +0300

    hidden_4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in dir(hidden_4):
-        if (i[:2] != '__'):
+    for i in dir(hidden_4):#use dir to search for the names in hidden_4
+        if (i[:2] != '__'):#check if i starts with __
             print(i)
