18d0d70d168634a433b8b664965b270e6686acc3
Kitemange <bryanmutheke18@gmail.com>
2022-06-16 00:39:12 +0300

    print number of elelments in a dictionary

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -5,8 +5,5 @@ def number_keys(a_dictionary):
     """
     A function that returns the number of keys in a dictionary.
     """
-    count = 0
-    #.items() returns number of keys in a dictionary
-    for key in a_dictionary.items():
-        count += 1
-    return count
+
+    return len(a_dictionary)
