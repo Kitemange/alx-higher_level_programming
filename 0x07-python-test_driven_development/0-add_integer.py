8d1c5cb072b9a1c7d1724fe85b7cc5a9d34a97a6
Kitemange <bryanmutheke18@gmail.com>
2022-07-04 19:34:54 +0300

    [Task] to add 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """Integers addition"""
 
-
 def add_integer(a, b=98):
-    """Adds two numbers
-    Args:
-        a - first number input
-        b - second number input
+    """Function to add two numbers
 
+    args:
+        a = first input
+        b = second input
+    
+    Returns:
+        The addition of the valuea a and b
+    
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
     """
-    if type(a) not in [int, float]:
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    
+    a = int(a)
+    b = int(b)
+    return a + b
