521322eac7e0c2c415a35f563ff64deefd7ec5f8
Kitemange <bryanmutheke18@gmail.com>
2022-07-13 13:04:06 +0300

    negative and equals magic functions

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,8 +7,8 @@ class MyInt(int):
     
     def __eq__(self, __x: object):
         """Not equating the operator"""
-        return super().__eq__(__x)
+        return super().__ne__(__x)
     
     def __ne__(self, __x: object):
         """Equating the operator"""
-        return super().__ne__(__x)
+        return super().__eq__(__x)
