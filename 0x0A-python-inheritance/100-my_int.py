7a9c687fa34a4693641803c239be978eae9b45a5
Kitemange <bryanmutheke18@gmail.com>
2022-07-13 13:05:17 +0300

    Trim white spaces

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,11 +4,11 @@
 
 class MyInt(int):
     """A class that inherits from int"""
-    
+
     def __eq__(self, __x: object):
         """Not equating the operator"""
         return super().__ne__(__x)
-    
+
     def __ne__(self, __x: object):
         """Equating the operator"""
         return super().__eq__(__x)
