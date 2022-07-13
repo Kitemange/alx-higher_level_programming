711ca5118cbc9292e161232bf59cb31382f9c802
Kitemange <bryanmutheke18@gmail.com>
2022-07-13 11:10:06 +0300

    [Task 7]:>Check for conditions in the basegeometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""Integer validator
-Author: Kanneh
+"""Geometry module.
 
+Author: Bryan Mutheke
 """
 
 
 class BaseGeometry:
-    """An empty class"""
+    """An empty class."""
 
     def area(self):
-        """Raises an exception because...
-        area is not implemented
+        """Raise an exception.
 
+        Raises:
+            Exception: uni,plemented area
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates the inputs: name and values
+        """Validate the inputs.
 
         Args:
-            name(str) - input name as string
-            value(int): pararams validator
+            name(str) : arg 1
+            value (int): arg 2
 
+        Raises:
+            TypeError: Not an integer
+            ValueError: Less than 0
+
+        Returns:
+            _type_: Value
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{} name must be greater than 0".format(name))
         return value
