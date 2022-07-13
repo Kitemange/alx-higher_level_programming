3d8ac4f84156511bb8d0163711f3b6cd8ac7a508
Kitemange <bryanmutheke18@gmail.com>
2022-07-13 11:48:25 +0300

    Integer validator

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,39 +1,6 @@
 #!/usr/bin/python3
-"""Geometry module.
-
-Author: Bryan Mutheke
-"""
-
-
-class BaseGeometry:
-    """An empty class."""
-
-    def area(self):
-        """Raise an exception.
-
-        Raises:
-            Exception: uni,plemented area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate the inputs.
-
-        Args:
-            name(str) : arg 1
-            value (int): arg 2
-
-        Raises:
-            TypeError: Not an integer
-            ValueError: Less than 0
-
-        Returns:
-            _type_: Value
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} name must be greater than 0".format(name))
+""" importing class BaseGeometry """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
