9a8d3b4eccdab6964640f71d121e0198882e7e32
Kitemange <bryanmutheke18@gmail.com>
2022-07-13 11:44:57 +0300

    [validator]:> Using integer_validator("y", y)

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,39 +1,52 @@
 #!/usr/bin/python3
-"""Integer validator
-Author: Kanneh
+"""Geometry module.
 
+Author: Bryan Mutheke
 """
 
 
 class BaseGeometry:
-    """A Base Geometry class"""
+    """An empty class."""
 
     def area(self):
-        """Raises an exception because...
-        area is not implemented
+        """Raise an exception.
 
+        Raises:
+            Exception: uni,plemented area
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates the inputs: name and values
+        """Validate the inputs.
 
         Args:
-            name(str) - input name as string
-            value(int): pararams validator
+            name(str) : arg 1
+            value (int): arg 2
 
+        Raises:
+            TypeError: Not an integer
+            ValueError: Less than 0
+
+        Returns:
+            _type_: Value
         """
-        if type(value) not in [int]:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{} name must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """A class Rectangle that inherits from BaseGeometry"""
+    """A class Rectangle that inherits from BaseGeometry."""
 
     def __init__(self, width, height):
-        """Instantiation of width and height"""
-        self.integer_validator(width, height)
-        self.__width = width
+        """Instantiate of width and height.
+
+        Args:
+            width (int): Rectangle width
+            height (int): Rectangle height
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__height = height
+        self.__width = width
