9d07509a0c94f0b9e92e68d14eee8904a8f78005
Kitemange <bryanmutheke18@gmail.com>
2022-07-13 11:54:37 +0300

    fix module documentation

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-
-"""Defines a class Rectangle that inherits
-from the BaseGeometry class.
-
-"""
+""" importing class BaseGeometry """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """A class Rectangle that inherits
-    from the BaseGeometry
-
-    """
+    """A class Rectangle that inherits from BaseGeometry."""
 
     def __init__(self, width, height):
-        """Instantiation of width and height"""
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
+        """Instantiate of width and height.
 
-    def area(self):
-        """Returns the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Return the print() and str()
-        representation of a Rectangle.
-
+        Args:
+            width (int): Rectangle width
+            height (int): Rectangle height
         """
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width
