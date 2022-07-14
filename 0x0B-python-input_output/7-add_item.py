9775286eda4ab7e5ff31ceaf99c78b179a2e092d
Kitemange <bryanmutheke18@gmail.com>
2022-07-14 13:20:18 +0300

    load,add,save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,14 @@
 save them to a file
 """
 import sys
-import os.path
+from os import path
 
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 list = []
-if os.path.exists("add_item.json"):
+if path.exists("add_item.json"):
     list = load_from_json_file("add_item.json")
 
 for arg in sys.argv[1:]:
