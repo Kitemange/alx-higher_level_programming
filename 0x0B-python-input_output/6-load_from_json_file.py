3c75f609bd6b9c427ae81c3bd0474f7f64590f35
Kitemange <bryanmutheke18@gmail.com>
2022-07-14 12:42:23 +0300

    [Return]:> Load for load_from_json_file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Create object from a JSON file"""
+"""write an Object to a text file, using a JSON representation."""
 import json
 
 
 def load_from_json_file(filename):
-    """A function that creates an Object
-    from a “JSON file”
-
-    """
-    with open(filename) as f:
+    """JSON representation."""
+    with open(filename, mode='w', encoding='utf-8')as f:
         return json.load(f)
