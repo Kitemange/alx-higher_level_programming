142d456ed05bf056cde589bdb1f018ea0c1af218
Kitemange <bryanmutheke18@gmail.com>
2022-11-03 13:30:52 +0300

    [FEATURES]:detailed docstring             and better copy method

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-
-
 def new_in_list(my_list, idx, element):
     """
     A function that replaces an element in a list at
     a specific position without modifying the original list
+
+    Args:
+        my_list ():contains a list of numbers
+        idx (): POsition to alter in the list
+        element (): Value to be placed in the idx
+
+    Returns:
+        copy: copy of my_list containing elelmt in the idx position
+        my_list: original list
     """
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return copy
+    else:
+        copy = my_list.copy()
+        copy[idx] = element
+        return copy
